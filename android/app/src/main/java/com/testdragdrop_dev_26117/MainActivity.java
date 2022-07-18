6403f0df3fd4a0d24838ddd923313aa956b521f4
Crowdbotics <engineering@crowdbotics.com>
2022-07-18 07:20:55 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/testdragdrop_dev_26117/MainActivity.java
@@ -0,0 +1,15 @@
+package com.testdragdrop_dev_26117;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "testdragdrop_dev_26117";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "testdragdrop_dev_26117",
+  url: "https://testdragdrop_dev_26117.botics.co",
+  api: "https://testdragdrop_dev_26117.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
